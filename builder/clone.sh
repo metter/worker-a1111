52b1ade2628bed17008604a93581d15ce8c71119
metter <markus.etter@gmail.com>
2023-07-28 20:12:21 +0200

    head

--- builder/clone.sh
+++ builder/clone.sh
@@ -6,6 +6,6 @@ mkdir -p /repositories/"$1"
 cd /repositories/"$1"
 git init
 git remote add origin "$2"
-git fetch origin master --depth=1   # fetch the latest commit from the master branch
-git reset --hard FETCH_HEAD         # reset to the latest fetched commit
+git fetch origin HEAD --depth=1   # fetch the latest commit from the default branch
+git reset --hard FETCH_HEAD       # reset to the latest fetched commit
 rm -rf .git
