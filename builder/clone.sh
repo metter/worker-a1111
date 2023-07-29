07d1f961942b1ce8d1c53702c75b75e2096172e4
metter <markus.etter@gmail.com>
2023-07-29 19:49:35 +0200

    lates default branch

--- builder/clone.sh
+++ builder/clone.sh
@@ -6,6 +6,6 @@ mkdir -p /repositories/"$1"
 cd /repositories/"$1"
 git init
 git remote add origin "$2"
-git fetch origin "$3" --depth=1
-git reset --hard "$3"
+git fetch origin --depth=1
+git checkout "$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')"
 rm -rf .git
