07846193dad4dd3c716b927d591f5d4d7e846aad
metter <markus.etter@gmail.com>
2023-10-20 21:43:47 +0200

    --skip-version-check --no-half-vae --no-hashing

--- builder/webui.sh
+++ builder/webui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start webui.py in the background
-python /stable-diffusion-webui/webui.py --no-half --ckpt /stable-diffusion-webui/model.safetensors --api &
+python /stable-diffusion-webui/webui.py --no-half --ckpt /stable-diffusion-webui/model.safetensors --api --skip-python-version-check --skip-torch-cuda-test --skip-version-check --no-half-vae --no-hashing &
 
 # Sleep for 35 seconds
 sleep 35
