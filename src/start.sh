ea7bf76f3a700887b0bfc4ad3ccd9ded90e3feca
metter <markus.etter@gmail.com>
2023-10-23 18:52:39 +0200

    vae path

--- src/start.sh
+++ src/start.sh
@@ -5,7 +5,7 @@ echo "Worker Initiated"
 echo "papertrail initialised"
 
 echo "Starting WebUI API"
-python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --ckpt /stable-diffusion-webui/model.safetensors --medvram-sdxl --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check --no-half-vae --no-hashing --no-download-sd-model &
+python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --ckpt /stable-diffusion-webui/model.safetensors ---vae-path /stable-diffusion-webui/models/VAE/sdxl_vae.safetensors -medvram-sdxl --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check --no-half-vae --no-hashing --no-download-sd-model &
 
 echo "Starting RunPod Handler"
 python -u /rp_handler.py
