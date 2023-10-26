2af2ada880a49dbc33f090d992213fcafbf98c9b
metter <markus.etter@gmail.com>
2023-10-26 12:34:48 +0200

    rp_handler local

--- src/start.sh
+++ src/start.sh
@@ -8,7 +8,7 @@ echo "Starting WebUI API"
 python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --ckpt /stable-diffusion-webui/model.safetensors --medvram-sdxl --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check --no-half-vae --no-hashing --no-download-sd-model &
 
 echo "Starting RunPod Handler"
-python -u /rp_handler.py --test_input '{"input": {"prompt": "this is a test"}}'
+python -u /rp_handler.py
 
 
    
