0cbaf2bc05ef51e6336258ce758a074f06fe8a05
metter <markus.etter@gmail.com>
2024-02-17 16:56:40 +0100

    sed

--- builder/webui.sh
+++ builder/webui.sh
@@ -5,7 +5,7 @@ echo "Debug: Current directory: $(pwd)"
 echo "Debug: Contents of current directory:"
 ls -l
 
-Rsed -i 's/from torchvision.transforms.functional_tensor import rgb_to_grayscale/from torchvision.transforms.functional import rgb_to_grayscale/' /usr/local/lib/python3.10/site-packages/basicsr/data/degradations.py
+sed -i 's/from torchvision.transforms.functional_tensor import rgb_to_grayscale/from torchvision.transforms.functional import rgb_to_grayscale/' /usr/local/lib/python3.10/site-packages/basicsr/data/degradations.py
 
 # Start webui.py in the background
 python /stable-diffusion-webui/webui.py --no-half --ckpt /model.safetensors --api --skip-python-version-check --skip-torch-cuda-test --skip-version-check --no-half-vae --no-hashing &
