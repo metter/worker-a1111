3b6d3ec2613ed2d750afcc2aaccb7688ef9fa428
Markus Etter <markus.ettergmail.com>
2024-08-25 11:23:31 +0200

    more

---
+++ start_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Kill any process running on port 3000
+echo "Killing any process running on port 3000..."
+lsof -t -i :3000 | xargs -r kill -9
+
+# Wait for the processes to be terminated
+sleep 2
+
+# Start the WebUI API
+echo "Starting WebUI API"
+# python stable-diffusion-webui/webui.py --listen --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt stable-diffusion-webui/model.safetensors --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --skip-version-check --no-hashing --no-download-sd-model --no-half --medvram --xformers &
+python stable-diffusion-webui/webui.py --listen --skip-install --ckpt stable-diffusion-webui/model.safetensors --port 3000 --api --skip-version-check --no-hashing --no-download-sd-model --no-half --medvram --xformers &
